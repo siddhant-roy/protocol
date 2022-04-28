cb15149e645d86005fcaf4e052a70a73bb9934ca
Siddhant Roy <53117868+siddhant-roy@users.noreply.github.com>
2022-04-28 18:39:24 +0530

    Initial

---
+++ config.js
@@ -0,0 +1,6 @@
+var config = {};
+
+config.provider = "https://broken-crimson-glitter.quiknode.pro/4f9d0f8b13786e6c6dcab61ac4518a3561ec2b3c/";
+config.mongodb = "mongodb://sam:samisop@35.236.8.167/trDB?authSource=admin";
+
+module.exports = config;
